553b00981e8ac0fff1c26e0163c40d5b058ff8e7
iamropo <iamropo@gmail.com>
2017-07-08 20:09:54 +0530

    aura logo

--- js/scroll.js
+++ js/scroll.js
@@ -2,10 +2,12 @@ const body = document.querySelector('body')
 const nav = document.querySelector('nav')
 
 function toggleNavigationBarOpacity () {
-	if (body.scrollTop > 100) {
+	if (body.scrollTop > 10) {
 		nav.style.backgroundColor = '#ffffff'
-	} else if (body.scrollTop < 100) {
+		nav.style.borderBottom = '1px solid #D3D3D3'
+	} else if (body.scrollTop < 10) {
 		nav.style.backgroundColor = ''
+		nav.style.borderBottom = ''
 	}
 }
 
